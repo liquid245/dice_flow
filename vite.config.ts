6e02dd79fa7f495b9f594d758617df24ce29a74e
Georgy Arutunyan <baegon@Georgys-MacBook-Air.local>
2026-08-27 21:58:21 +0300

    fix(pwa): use master die icon on all platforms, drop maskable

--- vite.config.ts
+++ vite.config.ts
@@ -41,10 +41,10 @@ export default defineConfig({
             purpose: 'any',
           },
           {
-            src: 'icons/maskable-512.png',
+            src: 'icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable',
+            purpose: 'any',
           },
         ],
       },
